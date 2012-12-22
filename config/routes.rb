de93402d9df9e463490c8fcdc89cecedb1fa7e83
raycoding <rayshuddhashil@gmail.com>
2012-12-23 00:19:32 +0530

    Socialbeam Rebooted 1.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,63 @@
 Socialbeam::Application.routes.draw do
 
-  resources :scribbles
+  resources :scribbles do
+    resources :scribble_comments
+  end
   resources :users
-  resources :sessions
-  root :to => 'browse#home'
-  get  "refresh"  => "browse#refreshscribbles", :as => "refresh"
-  get "votedup"  => "browse#votedup", :as => "votedup"
-  get  "voteddown"  => "browse#voteddown", :as => "voteddown"
+
+  resources :messages do
+    collection do
+      get 'compose', :to=>'messages#new', :as=>:compose
+      get 'index', :to=> 'messages#index', :as=> :index
+      get 'sent', :to=> 'messages#sent', :as=> :sent
+      post 'reply', :to=> 'messages#reply', :as=> :reply
+      post 'trash', :to=> 'messages#trash', :as=> :trash
+    end
+  end
+
+  resource :socialbeams do
+    collection do
+      get 'home'
+      get 'loadmorescribbles'
+    end
+  end
   
-  #Sessions Users
-  get "logout_user" => "sessions#destroy", :as => "logout_user"
-  post "login_user" => "sessions#new", :as => "login_user"
- 
- #Users
+  resources :user_sessions
+  resources :authorizations
+  match '/auth/:provider/callback' => 'authorizations#create'
+  match '/auth/failure' => 'authorizations#failure'
+  match '/auth/:provider' => 'authorizations#blank'
+
+  
+  resources :newsfeeds
+  root :to => 'socialbeams#home'
+  resources :chats
+  get  '/sbchat' => 'chats#index', :as => :chat
+  post '/new_message' => 'chats#new_message', :as => :new_message
+  
+  get  "refresh"  => "socialbeams#refreshscribbles", :as => "refresh"
+  get "votedup"  => "socialbeams#votedup", :as => "votedup"
+  get  "voteddown"  => "socialbeams#voteddown", :as => "voteddown"
+
+  #Sessions & Users & Profile
+  get "logout_user" => "user_sessions#destroy", :as => "logout_user"
+  get "login_user" => "user_sessions#new", :as => "login_user"
   get "signup" => "users#new", :as => "signup"
+  match "/myconnections/:beamer_id" => "users#showconnections", :as=>"myconnections"
+  match :profile, :to => 'user_profile#index', :as=>:about
+  match 'user_profile/update/:beamer_id', :to => 'user_profile#update', :as => :update_beamer_profile, :via => [:post]
+
+  resources :friendships do
+    collection do
+      get 'req',:as=>"addfriend"
+      get 'accept',:as=>"accept_fr"
+      get 'reject',:as=>"reject_fr"
+    end
+  end
+ 
+  
+
+  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -67,5 +111,4 @@ Socialbeam::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
 end
